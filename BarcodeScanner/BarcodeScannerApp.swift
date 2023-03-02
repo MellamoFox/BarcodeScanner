c71e9e3b83f6e90164bfe7ca5d11c412657c9ad1
MellamoFox <tsoi.v87@icloud.com>
2023-03-02 12:01:14 +0200

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by MellamoFox on 02.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
